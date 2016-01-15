e21cb3b66d711cade60e91aa59df9ff0d900542d
wgb <worth.becker@gmail.com>
2016-01-15 14:28:21 -0500

    Respond to bad requests with 400 - Bad Request

--- src/pz_discover/controllers/services.clj
+++ src/pz_discover/controllers/services.clj
@@ -3,14 +3,18 @@
             [pz-discover.views.services :as sv]))
 
 (defn lookup-svc-type [zookeeper type]
-  (if-let [data (sm/by-type zookeeper type)]
-    (sv/shape-response 200 :body data)
-    (sv/shape-response 404)))
+  (if type
+    (if-let [data (sm/by-type zookeeper type)]
+      (sv/shape-response 200 :body data)
+      (sv/shape-response 404))
+    (sv/shape-response 400 :body {:reason "Resource type required."})))
 
 (defn lookup-svc [zookeeper name]
-  (if-let [data (sm/by-name zookeeper name)]
-    (sv/shape-response 200 :body data)
-    (sv/shape-response 404)))
+  (if name
+    (if-let [data (sm/by-name zookeeper name)]
+      (sv/shape-response 200 :body data)
+      (sv/shape-response 404))
+    (sv/shape-response 400 :body {:reason "Resource name required."})))
 
 (defn lookup-all [zookeeper]
   (if-let [data (sm/all-names zookeeper)]
@@ -20,18 +24,24 @@
 (defn register-svc [zookeeper request]
   (let [name (-> request :params :name)
         data (-> request :params :data)]
-    (if-let [result (sm/register-by-name zookeeper name data)]
-      (sv/shape-response 201 :body result)
-      (sv/shape-response 303 :headers {"Location" (format "/api/v1/resources/%s" name)}))))
+    (if (and name data)
+      (if-let [result (sm/register-by-name zookeeper name data)]
+        (sv/shape-response 201 :body result)
+        (sv/shape-response 303 :headers {"Location" (format "/api/v1/resources/%s" name)}))
+      (sv/shape-response 400 :body {:reason "Resource name and data required."}))))
 
 (defn update-svc [zookeeper request]
   (let [name (-> request :params :name)
         data (-> request :params :data)]
-    (if-let [result (sm/update-by-name zookeeper name data)]
-      (sv/shape-response 200 :body result)
-      (sv/shape-response 404))))
+    (if (and name data)
+      (if-let [result (sm/update-by-name zookeeper name data)]
+        (sv/shape-response 200 :body result)
+        (sv/shape-response 404))
+      (sv/shape-response 400 :body {:reason "Resource name and data required."}))))
 
 (defn delete-svc [zookeeper name]
-  (if-let [result (sm/delete-by-name zookeeper name)]
-    (sv/shape-response 200)
-    (sv/shape-response 404)))
+  (if name
+    (if-let [result (sm/delete-by-name zookeeper name)]
+      (sv/shape-response 200)
+      (sv/shape-response 404))
+    (sv/shape-response 400 :body {:reason "Resource name required."})))
