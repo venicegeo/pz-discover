b4c16d813b7d14fb9a6e50d30658c9d546286101
Seth Baxter <sethbaxter@prominentedge.com>
2016-03-11 04:40:28 -0500

    Tweak artifact name

--- scripts/vars.sh
+++ scripts/vars.sh
@@ -3,5 +3,5 @@
 APP=pz-discover
 EXT=jar
 VERSION=$(git rev-parse --short HEAD)
-ARTIFACT=$APP.$VERSION.$EXT
+ARTIFACT=$APP-$VERSION.$EXT
 NEXUSURL=https://nexus.devops.geointservices.io/content/repositories/Piazza
